ceeeda51aae88f5b839eab85aec9b66d1a60192e
luqingxin <858184471@qq.com>
2022-02-10 22:59:52 +0800

    呵呵

--- test.cpp
+++ test.cpp
@@ -5,10 +5,26 @@
 #include<cstdlib>
 #include<arpa/inet.h>
 #include <netinet/in.h>
+#include<algorithm>
+
 
 using namespace std;
 
 const unsigned short defaltPort = 27123;
+
+char *itoa(int x){
+    char *c ;
+    c = (char *)malloc(sizeof(char)*100);
+    int n;
+    for(n=0;x;n++){
+        c[n] = x%10;
+        x/=10;
+    }
+    for(int i=0;i<n;i++){
+        swap(c[i],c[n-i-1]);
+    }
+    return c;
+}
 
 int main(){
     int serviceSock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
@@ -28,7 +44,11 @@ int main(){
     memset(&targetAddr,0,sizeof(targetAddr));
     targetSock = accept(serviceSock,(struct sockaddr*)&targetAddr,&targetAddrLen);
 
-    printf("");
+    
+    send(targetSock,inet_ntoa(targetAddr.sin_addr),sizeof(inet_ntoa(targetAddr.sin_addr)),0);
+    send(targetSock,itoa(targetAddr.sin_port),sizeof(itoa(targetAddr.sin_port)),0);
 
+    close(serviceSock);
+    close(targetSock);
     return 0;
 }
